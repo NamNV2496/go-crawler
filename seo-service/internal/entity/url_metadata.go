718e652c8af005eb887c64cd594bc0a4e37e0de1
namnv2496 <namnv2496@gmail.com>
2025-06-26 21:54:19 +0700

    reverse

--- seo-service/internal/entity/url_metadata.go
+++
@@ -1,8 +0,0 @@
-package entity
-
-type UrlMetadata struct {
-	Id      int64  `json:"id,omitempty"`
-	UrlId   int64  `json:"url_id,omitempty"`
-	Keyword string `json:"keyword,omitempty"`
-	Value   string `json:"value,omitempty"`
-}
